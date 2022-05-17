da5f31d81947ee1696eaa67e93d166a7c6dd8c8b
Ruta Gradzevice <ruta.gradzevice@gmail.com>
2022-05-17 18:37:31 +0300

    Finished Task4

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,26 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const divEl = document.getElementById("output");
+const msg = document.getElementById("message");
+
+async function processCars() {
+  const response = await fetch(ENDPOINT);
+  const data = await response.json();
+
+  data.forEach((carManu) => {
+    const brandEl = document.createElement("p");
+    const br = document.createElement("br");
+    const modelsEl = document.createElement("p");
+    brandEl.textContent = `Manufacturer: ${carManu.brand}`;
+    modelsEl.textContent = `Models: ${carManu.models.join(", ")}`;
+    divEl.appendChild(brandEl);
+
+    divEl.appendChild(modelsEl);
+    divEl.appendChild(br);
+  });
+}
+
+processCars();
